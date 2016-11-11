989363928104f5a53940b8cd2aca99ba32c1d72a
sbulygin <sergteletrade@gmail.com>
2016-11-11 19:05:45 +0300

    add Calculate

---
+++ Chapter_001/src/main/java/ru/sbulygin/Calculate.java
@@ -0,0 +1,18 @@
+package ru.sbulygin;
+
+/**
+* Colculate class.
+* @author sbulygin
+* @since 10.11.2016
+* @version 1.0
+*/
+public class Calculate {
+    /**
+    * Main
+    * @param arg - arg
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello World");     
+	}	
+}
+
