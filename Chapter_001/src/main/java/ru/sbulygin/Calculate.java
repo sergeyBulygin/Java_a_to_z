9b9d834cd2798d9288f7f800820b2f758e766cd5
sbulygin <sergteletrade@gmail.com>
2016-11-12 13:16:13 +0300

    correct Calculate

--- Chapter_001/src/main/java/ru/sbulygin/Calculate.java
+++ Chapter_001/src/main/java/ru/sbulygin/Calculate.java
@@ -2,17 +2,16 @@ package ru.sbulygin;
 
 /**
 * Colculate class.
-* @author sbulygin
-* @since 10.11.2016
-* @version 1.0
+* @author sbulygin.
+* @since 10.11.2016.
+* @version 1.0.
 */
 public class Calculate {
-    /**
-    * Main
-    * @param arg - arg
-    */
-    public static void main(String[] args) {
-        System.out.println("Hello World");     
-	}	
-}
-
+     /**
+     * Main.
+     * @param args do not use.
+     */
+     public static void main(String[] args) {
+         System.out.println("Hello World");
+  }
+ }
