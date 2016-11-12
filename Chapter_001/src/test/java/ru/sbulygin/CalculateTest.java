7a1db93aa15f6f6e78a34ee85f6fd6200a6969b8
sbulygin <sergteletrade@gmail.com>
2016-11-12 13:18:36 +0300

    correct CalculateTest

--- Chapter_001/src/test/java/ru/sbulygin/CalculateTest.java
+++ Chapter_001/src/test/java/ru/sbulygin/CalculateTest.java
@@ -1,6 +1,5 @@
 package ru.sbulygin;
 
-
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -8,22 +7,21 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test
+* Test.
 *
-* @author Sergey Bulygin (sergteletrade@gmail.com)
-* @version 11.11.2016
-* @since 0.1
+* @author Sergey Bulygin (sergteletrade@gmail.com).
+* @version 11.11.2016.
+* @since 0.1.
 */
 public class CalculateTest {
-	/**
-	* Test add.
-	*/
-	@Test
-	public void whenAddOneToOneThenTwo() {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
-		Calculate.main(null);
-		assertThat(out.toString(), is("Hello World\r\n"));
-	}	
-}	
-
+    /**
+    * Test add.
+    */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello World\r\n"));
+  }
+ }
