adf02619b6e696804c20aff2cbbcac1876e6d6e3
sbulygin <sergteletrade@gmail.com>
2016-11-18 18:01:56 +0300

    add MaxTest

---
+++ Chapter_001/src/test/java/ru/sbulygin/MaxTest.java
@@ -0,0 +1,40 @@
+package ru.sbulygin;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Sergey Bulygin (sergteletrade@gmail.com).
+* @version 1.0.
+* @since 18.11.2016.
+*/
+public class MaxTest {
+
+    /**
+    * Тест для класса Max.
+    *
+    * Тест метода max при равных значениях.
+    */
+    @Test
+    public void whenFirstEqualSecondThenNull() {
+        final Max value = new Max();
+        final int result = value.max(10, 10);
+        assertThat(result, is(0));
+    }
+
+    /**
+    * Тест для класса Max.
+    *
+    * Тест метода max .
+    */
+    @Test
+    public void whenFirstComparSecondThenTen() {
+        final Max value = new Max();
+        final int result = value.max(10, 5);
+        final int expectResult = 10;
+        assertThat(result, is(expectResult));
+    }
+ }
