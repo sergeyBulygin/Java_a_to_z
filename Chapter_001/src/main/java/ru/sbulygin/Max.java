f17f2986a3d923592d132acbd674d2e1efb19a84
sbulygin <sergteletrade@gmail.com>
2016-11-19 00:24:08 +0300

    new Max

--- Chapter_001/src/main/java/ru/sbulygin/Max.java
+++ Chapter_001/src/main/java/ru/sbulygin/Max.java
@@ -13,57 +13,6 @@ package ru.sbulygin;
 public class Max {
 
     /**
-    * Добавляем поле int first.
-    */
-    private int first;
-    /**
-    * Добавляем поле int second.
-    */
-    private int second;
-
-    /**
-    * Добавляем поле int third.
-    */
-    private int third;
-
-    /**
-    * Добавляем поле int value.
-    */
-    private int value;
-
-    /**
-    * Метод getValue.
-    * @return полученое значение value.
-    */
-    public int getValue() {
-        return this.value;
-    }
-
-    /**
-    * Метод getFirst.
-    * @return полученое значение first.
-    */
-    public int getFirst() {
-        return this.first;
-    }
-
-    /**
-    * Метод getSecond.
-    * @return полученое значение second.
-    */
-    public int getSecond() {
-        return this.second;
-    }
-
-    /**
-    * Метод getThird.
-    * @return полученое значение third.
-    */
-    public int getThird() {
-        return this.third;
-    }
-
-    /**
     * Метод max.
     * Сравнивает 2 числа.
     * @param first первое число.
@@ -71,28 +20,25 @@ public class Max {
     * @return максимальное значение
     */
     public int max(int first, int second) {
-        if (first == second) {
-            return 0;
-        } else if (first > second) {
-            return first;
+        int result;
+        if (first > second) {
+            result = first;
         } else {
-            return second;
+            result = second;
         }
+        return result;
     }
 
     /**
-    * Метод maxThree.
+    * Метод max.
     * Сравнивает 3 числа.
     * @param first первое число.
     * @param second второе число.
     * @param third третье число.
     * @return максимальное значение
     */
-    public int maxThree(int first, int second, int third) {
-        if (first == second || second == third || first == third) {
-            return 0;
-        } else {
-            return Math.max(first, Math.max(second, third));
+    public int max(int first, int second, int third) {
+        Max calculateMax = new Max();
+        return calculateMax.max(first, calculateMax.max(second, third));
         }
-    }
  }
