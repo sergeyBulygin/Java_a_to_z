9cfbcb9a91f1a741ebfddc6dae2cc4b32d9de961
sbulygin <sergteletrade@gmail.com>
2016-11-28 12:32:31 +0300

    add FactorialTest

---
+++ Chapter_001/src/test/java/ru/sbulygin/FactorialTest.java
@@ -0,0 +1,41 @@
+package ru.sbulygin;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Sergey Bulygin (sergteletrade@gmail.com).
+* @version 1.0.
+* @since 28.11.2016.
+*/
+public class FactorialTest {
+
+    /**
+    * Тест для класса Factorial.
+    *
+    * Тест метода fact .
+    */
+    @Test
+    public void whenAddCorrectValueThenReturnFactorial() {
+        Factorial factorial = new Factorial();
+        final long result = factorial.fact(3);
+        final long expectResult = 6L;
+        assertThat(result, is(expectResult));
+    }
+
+    /**
+    * Тест для класса Max.
+    *
+    * Тест метода max.
+    */
+    @Test
+    public void whenAddNullThenReturnOne() {
+        Factorial factorial = new Factorial();
+        final long result = factorial.fact(0);
+        final long expectResult = 1L;
+        assertThat(result, is(expectResult));
+    }
+ }
