a7b46a1d9fb31304fba32f1949cc8c3361ba76ae
sbulygin <sergteletrade@gmail.com>
2016-12-06 15:52:08 +0300

    add Turn

---
+++ Chapter_001/src/main/java/ru/sbulygin/Turn.java
@@ -0,0 +1,29 @@
+package ru.sbulygin;
+
+/**
+* Class Turn.
+* @author sbulygin.
+* @since 06.12.2016.
+* @version 1.0.
+*/
+
+public class Turn {
+
+    /**
+    * Method back.
+    * This method returns an array of inverted.
+    * @param arr array of integer values.
+    * @return inverted array.
+    */
+    public static int[] back(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            throw new IllegalArgumentException("Array cannot have lenght <= 1 or equal to 0.");
+        }
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmpCell = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = tmpCell;
+        }
+        return arr;
+    }
+ }
