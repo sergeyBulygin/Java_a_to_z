8a22df08a97ad0353a056752cd1fbfee47e44f7b
sbulygin <sergteletrade@gmail.com>
2017-01-06 14:52:07 +0300

    correct pom

--- Chapter_001/src/main/java/ru/sbulygin/Calculate.java
+++ Chapter_001/src/main/java/ru/sbulygin/Calculate.java
@@ -1,17 +1,18 @@
 package ru.sbulygin;
 
 /**
-* Colculate class.
-* @author sbulygin.
-* @since 10.11.2016.
-* @version 1.0.
-*/
+ * Colculate class.
+ * @author sbulygin.
+ * @since 10.11.2016.
+ * @version 1.0.
+ */
 public class Calculate {
-     /**
+    /**
      * Main.
      * @param args do not use.
      */
-     public static void main(String[] args) {
-         System.out.println("Hello World!");
-  }
- }
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
