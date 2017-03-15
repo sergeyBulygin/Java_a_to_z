81105936d83f21256c2e07ea1823d6644ceca14b
sergeyBulygin <sergteletrade@gmail.com>
2017-03-15 16:31:23 +0300

    add Pawn class

---
+++ Chapter_002/Test_task/Game/src/main/java/ru/sbulygin/chees/Pawn.java
@@ -0,0 +1,56 @@
+package ru.sbulygin.chees;
+
+import ru.sbulygin.exeptions.ImpossibleMoveException;
+
+/**
+ * Class Pawn.
+ *
+ * @author ru.sbulygin.start.
+ * @since 15.03.2017.
+ * @version 1.0.
+ */
+public class Pawn extends Figure {
+
+    /**
+     * An array field of the movement of an pawn.
+     */
+    private Cell[] direction;
+
+    /**
+     * The first stroke of the figure.
+     */
+    private int firstMove;
+
+    /**
+     * The constructor of the Pawn class generates the class object.
+     * @param position position of the figure on the Board.
+     */
+    public Pawn(Cell position) {
+        super(position);
+    }
+
+    /**
+     * Way overridden method of the abstract class Figure.
+     * @param distance the specified cell on the Board.
+     * @return array of the path of movement of the pawn.
+     * @throws ImpossibleMoveException error of the impossibility of motion pawn.
+     */
+    @Override
+    public Cell[] way(Cell distance) throws ImpossibleMoveException {
+
+        int posX = getPosition().getPivotX();
+        int posY = getPosition().getPivotY();
+        int distX = distance.getPivotX();
+        int distY = distance.getPivotY();
+
+        if (firstMove == 2 || firstMove == 1) {
+            if (posX == distX && posY != distY) {
+                direction = motionDirection(distance);
+            } else {
+                throw new ImpossibleMoveException("Pawn can't walk.");
+            }
+
+        }
+        return direction;
+    }
+}
