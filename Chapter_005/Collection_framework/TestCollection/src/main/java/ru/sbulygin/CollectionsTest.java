6acf32b367a639bda17bfc523eb062675f5bd104
sergeyBulygin <sergteletrade@gmail.com>
2017-04-09 16:22:14 +0300

    add JavaDoc & registration code #1

--- Chapter_005/Collection_framework/TestCollection/src/main/java/ru/sbulygin/CollectionsTest.java
+++ Chapter_005/Collection_framework/TestCollection/src/main/java/ru/sbulygin/CollectionsTest.java
@@ -1,5 +1,6 @@
 package ru.sbulygin;
 
+
 import java.util.*;
 
 /**
@@ -11,6 +12,13 @@ import java.util.*;
  */
 public class CollectionsTest {
 
+    /**
+     * The method of adding items to the collection for test.
+     * @param collection testing collection.
+     * @param line element for testing collection.
+     * @param amount the number of elements for testing collection.
+     * @return the test time.
+     */
     public long add(Collection<String> collection, String line, int amount) {
 
         long startTime = System.currentTimeMillis();
@@ -22,6 +30,12 @@ public class CollectionsTest {
         return finishTime - startTime;
     }
 
+    /**
+     * The method of delete items to the collection for test.
+     * @param collection testing collection.
+     * @param amount the number of elements for testing collection.
+     * @return the test time.
+     */
     public long delete(Collection<String> collection, int amount) {
 
         long startTime = System.currentTimeMillis();
@@ -39,32 +53,35 @@ public class CollectionsTest {
         return finishTime - startTime;
     }
 
-public static void main(String[] args) {
+    /**
+     * Method to test collections when adding and removing elements.
+     * @param args the command-line arguments.
+     */
+    public static void main(String[] args) {
 
-    CollectionsTest test = new CollectionsTest();
-    LinkedList<String> linkedList = new LinkedList<>();
-    ArrayList<String> arrayList = new ArrayList<>();
-    TreeSet<String> treeSet = new TreeSet<>();
+        CollectionsTest test = new CollectionsTest();
+        LinkedList<String> linkedList = new LinkedList<>();
+        ArrayList<String> arrayList = new ArrayList<>();
+        TreeSet<String> treeSet = new TreeSet<>();
 
-    System.out.println("Time adding elements: ");
-    System.out.println(String.format("LinkedList: %s ms", test.add(linkedList, "element", 7_000_000)));
-    System.out.println(String.format("arrayList: %s ms", test.add(arrayList, "element", 7_000_000)));
-    System.out.println(String.format("treeSet: %s ms", test.add(treeSet, "element", 7_000_000)));
-    linkedList.clear();
-    arrayList.clear();
-    treeSet.clear();
+        System.out.println("Time adding elements: ");
+        System.out.println(String.format("LinkedList: %s ms", test.add(linkedList, "element", 7_000_000)));
+        System.out.println(String.format("arrayList: %s ms", test.add(arrayList, "element", 7_000_000)));
+        System.out.println(String.format("treeSet: %s ms", test.add(treeSet, "element", 7_000_000)));
 
-    test.add(linkedList, "element", 180_000);
-    test.add(arrayList, "element", 180_000);
-    test.add(treeSet, "element", 180_000);
-    System.out.println();
-    System.out.println("Time of delete elements: ");
-    System.out.println(String.format("LinkedList: %s ms", test.delete(linkedList, 120_000)));
-    System.out.println(String.format("arrayList: %s ms", test.delete(arrayList, 120_000)));
-    System.out.println(String.format("treeSet: %s ms", test.delete(treeSet, 120_000)));
+        linkedList.clear();
+        arrayList.clear();
+        treeSet.clear();
 
+        test.add(linkedList, "element", 180_000);
+        test.add(arrayList, "element", 180_000);
+        test.add(treeSet, "element", 180_000);
 
-
-}
+        System.out.println();
+        System.out.println("Time of delete elements: ");
+        System.out.println(String.format("LinkedList: %s ms", test.delete(linkedList, 120_000)));
+        System.out.println(String.format("arrayList: %s ms", test.delete(arrayList, 120_000)));
+        System.out.println(String.format("treeSet: %s ms", test.delete(treeSet, 120_000)));
+    }
 
 }
