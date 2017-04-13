d366d0d5e707057847d7a680d10d97e24b7b628b
sergeyBulygin <sergteletrade@gmail.com>
2017-04-13 12:50:35 +0300

    Create method toArray #2

--- Chapter_005/Collection_framework/Conversion/src/main/java/ru/sbulygin/ConvertList.java
+++ Chapter_005/Collection_framework/Conversion/src/main/java/ru/sbulygin/ConvertList.java
@@ -1,6 +1,9 @@
 package ru.sbulygin;
 
+import sun.font.GlyphLayout;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -23,11 +26,35 @@ public class ConvertList {
             for (int value : index) {
                 arrayList.add(value);
             }
-            return arrayList;
         }
-
-
-
+        return arrayList;
 
     }
+
+    /**
+     * Method converts an ArrayList into a two-dimensional array.
+     * @param list collection.
+     * @param rows the number of rows.
+     * @return array.
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        int length = list.size() / rows;
+        if (list.size() % rows != 0) {
+            length += 1;
+        }
+        int[][] listArray = new int[rows][length];
+        Iterator<Integer> iterator = list.iterator();
+        for (int row = 0; row != rows; row++) {
+            for (int column = 0; column != length; column++) {
+                if (iterator.hasNext()) {
+                    listArray[row][column] = iterator.next();
+                } else {
+                    listArray[row][column] = 0;
+                }
+            }
+        }
+        return listArray;
+
+    }
+
 }
