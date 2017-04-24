5b90844658428331e900b5e915475a36522d1adb
sergeyBulygin <sergteletrade@gmail.com>
2017-04-24 18:11:20 +0300

    add CommentTest and UserTest

---
+++ Chapter_005/Generalizations/ChangeTracker/src/test/java/ru/sbulygin/models/CommentTest.java
@@ -0,0 +1,38 @@
+package ru.sbulygin.models;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Class CommentTest.
+ *
+ * @author ru.sbulygin.
+ * @since 24.04.2017.
+ * @version 1.0.
+ */
+public class CommentTest {
+
+    /**
+     * Test comment field.
+     */
+    private String testComment = "test";
+
+    /**
+     * Test instance comment field.
+     */
+    private Comment testComm = new Comment();
+
+    /**
+     * Test method class Comment.
+     * @throws Exception Exception when errors test.
+     */
+    @Test
+    public void whenAddCommentThatGetThisComment() throws Exception {
+        testComm.addComment(testComment);
+        assertThat(testComm.getRemark()[0], is(testComment));
+    }
+
+}
---
+++ Chapter_005/Sorting/SortUser/src/test/java/ru/sbulygin/UserTest.java
@@ -0,0 +1,65 @@
+package ru.sbulygin;
+
+import org.junit.Test;
+
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Class UserTest.
+ *
+ * @author ru.sbulygin.
+ * @since 24.04.2017.
+ * @version 1.0.
+ */
+public class UserTest {
+
+    /**
+     * Test age field.
+     */
+    private int testAge = 30;
+
+    /**
+     * Test name field.
+     */
+    private String testName = "name";
+
+    /**
+     * Test instance user field.
+     */
+    private User userTest = new User(testName, testAge);
+
+    /**
+     * Test constructor User class.
+     * @throws Exception Exception when errors test.
+     */
+    @Test
+    public void whenAddInConstructorAllThatGetAll() throws Exception {
+        assertThat(userTest.getName(), is(testName));
+        assertThat(userTest.getAge(), is(testAge));
+
+    }
+
+    /**
+     * Test getter and setter name field User class.
+     * @throws Exception Exception when errors test.
+     */
+    @Test
+    public void whenSetNameThatGetThisName() throws Exception {
+        userTest.setName(testName);
+        assertThat(userTest.getName(), is(testName));
+    }
+
+    /**
+     * Test getter and setter age field User class.
+     * @throws Exception Exception when errors test.
+     */
+    @Test
+    public void whenSetAgeThatGetThisAge() throws Exception {
+        userTest.setAge(testAge);
+        assertThat(userTest.getAge(), is(testAge));
+    }
+
+}
