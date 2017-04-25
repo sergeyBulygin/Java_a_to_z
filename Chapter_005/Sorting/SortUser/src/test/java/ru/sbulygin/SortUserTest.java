75520d271f6a4dccafce95d2342785ab59819c99
sergeyBulygin <sergteletrade@gmail.com>
2017-04-25 13:22:35 +0300

    Create class SortUserTest & test sort method #6

--- Chapter_005/Sorting/SortUser/src/test/java/ru/sbulygin/SortUserTest.java
+++ Chapter_005/Sorting/SortUser/src/test/java/ru/sbulygin/SortUserTest.java
@@ -2,7 +2,14 @@ package ru.sbulygin;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 
 /**
  * Class SortUserTest.
@@ -13,9 +20,38 @@ import static org.junit.Assert.*;
  */
 public class SortUserTest {
 
+    /**
+     * New user for test.
+     */
+    private User testUserOne = new User("Sergey", 39);
 
+    /**
+     * New user for test.
+     */
+    private User testUserTwo = new User("Sasha", 20);
+
+    /**
+     * New user for test.
+     */
+    private User testUserTree = new User("Victoria", 18);
+
+    /**
+     * Test sort method of class SortUser.
+     * @throws Exception Exception when errors test.
+     */
     @Test
-    public void sort() throws Exception {
+    public void whenAddUserListThatGetSortTreeCollection() throws Exception {
+        List<User> testUserList = Arrays.asList(testUserOne, testUserTwo, testUserTree);
+        SortUser testSortUser = new SortUser();
+        Set<User> testUserSet = new TreeSet<>();
+        testUserSet.add(testUserOne);
+        testUserSet.add(testUserTwo);
+        testUserSet.add(testUserTree);
+        assertThat(testUserSet, is(testSortUser.sort(testUserList)));
+
+
+
+
 
 
 
