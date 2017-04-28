fd0cdeac5c82118e565061d5a34efa80233d4e4d
sergeyBulygin <sergteletrade@gmail.com>
2017-04-28 15:08:54 +0300

    Create class User #7

---
+++ Chapter_005/Test_task_chapter_005/Bank/src/main/java/ru/sbulygin/Account.java
@@ -0,0 +1,11 @@
+package ru.sbulygin;
+
+/**
+ * Class Account.
+ *
+ * @author ru.sbulygin.
+ * @since 28.04.2017.
+ * @version 1.0.
+ */
+public class Account {
+}
---
+++ Chapter_005/Test_task_chapter_005/Bank/src/main/java/ru/sbulygin/User.java
@@ -0,0 +1,96 @@
+package ru.sbulygin;
+
+/**
+ * Class User.
+ *
+ * @author ru.sbulygin.
+ * @since 28.04.2017.
+ * @version 1.0.
+ */
+public class User {
+
+    /**
+     *  Users name field.
+     */
+    private String name;
+
+    /**
+     * Users passport field.
+     */
+    private String passport;
+
+    /**
+     * The constructor of the User class.
+     * @param name Users name.
+     * @param passport Users passport.
+     */
+    public User(String name, String passport) {
+        this.name = name;
+        this.passport = passport;
+    }
+
+    /**
+     * Getter users name.
+     * @return users name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter users name.
+     * @param name users name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Getter users passport.
+     * @return users passport.
+     */
+    public String getPassport() {
+        return passport;
+    }
+
+    /**
+     * Setter users passport.
+     * @param passport users passport.
+     */
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    /**
+     * Override equals.
+     * @param o object.
+     * @return Boolean comparison value.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        if (getName() != null ? !getName().equals(user.getName()) : user.getName() != null) {
+            return false;
+        }
+        return getPassport() != null ? getPassport().equals(user.getPassport()) : user.getPassport() == null;
+    }
+
+    /**
+     * Override hash code.
+     * @return result.
+     */
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getPassport() != null ? getPassport().hashCode() : 0);
+        return result;
+    }
+}
