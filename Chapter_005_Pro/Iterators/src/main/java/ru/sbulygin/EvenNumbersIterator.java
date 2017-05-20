377c9bf9d00623dc8a6e20bff421bf734cbac6dc
sergeyBulygin <sergteletrade@gmail.com>
2017-05-20 11:57:05 +0300

    EvenNumberIterator & EvenNumberIteratorTest class implementation #10

---
+++ Chapter_005_Pro/Iterators/src/main/java/ru/sbulygin/EvenNumbersIterator.java
@@ -0,0 +1,64 @@
+package ru.sbulygin;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Class EvenNumbersIterator.
+ *
+ * @author sbulygin.
+ * @since 19.05.2017.
+ * @version 1.0.
+ */
+public class EvenNumbersIterator implements Iterator {
+
+    /**
+     * Collection ArrayList.
+     */
+    private ArrayList<Integer> collection = new ArrayList<>();
+
+    /**
+     * Index of iterator.
+     */
+    private int index;
+
+    /**
+     * Value for next().
+     */
+    private int valueNext;
+
+    /**
+     * Constructor.
+     * @param collection collection.
+     */
+    EvenNumbersIterator(ArrayList<Integer> collection) {
+        this.collection = collection;
+    }
+
+    @Override
+    public boolean hasNext() {
+        int counter = 0;
+        for (Integer index : collection) {
+            if (index % 2 == 0) {
+                counter++;
+            }
+        }
+        return counter > this.valueNext;
+    }
+
+    @Override
+    public Object next() {
+        int result;
+        result = 0;
+        for (int value = index; value < collection.size(); value++) {
+            if (collection.get(value) % 2 == 0) {
+                result = collection.get(value);
+                index = value + 1;
+                valueNext++;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ Chapter_005_Pro/Iterators/src/test/java/ru/sbulygin/EvenNumbersIteratorTest.java
@@ -0,0 +1,71 @@
+package ru.sbulygin;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class EvenNumbersIteratorTest.
+ *
+ * @author sbulygin.
+ * @since 19.05.2017.
+ * @version 1.0.
+ */
+public class EvenNumbersIteratorTest {
+
+    /**
+     * Instance class EvenNumbersIterator for test.
+     */
+    private EvenNumbersIterator iteratorTest;
+
+    /**
+     * Collection for test.
+     */
+    private ArrayList<Integer> collectionTest;
+
+    /**
+     * The method contains information for tests.
+     */
+    @Before
+    public void infoForTestEvenNumbersIterator() {
+        collectionTest = new ArrayList<>();
+        collectionTest.add(1);
+        collectionTest.add(3);
+        collectionTest.add(2);
+        collectionTest.add(5);
+        collectionTest.add(4);
+        iteratorTest = new EvenNumbersIterator(collectionTest);
+    }
+
+    /**
+     * Test method next().
+     */
+    @Test
+    public void whenAddNumbersInCollectionThenResultEven() {
+        int expectedResult = 2;
+        int expectedResultTwo = 4;
+        int result = (Integer) iteratorTest.next();
+        int resultTwo = (Integer) iteratorTest.next();
+        assertThat(result, is(expectedResult));
+        assertThat(resultTwo, is(expectedResultTwo));
+    }
+
+    /**
+     * Test method hasNext().
+     */
+    @Test
+    public void whenAddNumbersInCollectionThenResultHasNextTrueOrFalse() {
+        iteratorTest.next();
+        iteratorTest.hasNext();
+        assertThat(iteratorTest.hasNext(), is(true));
+
+        iteratorTest.next();
+        assertThat(iteratorTest.hasNext(), is(false));
+
+
+    }
+}
